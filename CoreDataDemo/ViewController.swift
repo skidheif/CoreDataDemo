4d64d3afbda2987b4974ee2e349fcc3a09f67874
John Heifetz <skidheif@gmail.com>
2019-09-03 16:58:37 +0300

    resolve task

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,193 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Alexey Efimov on 02/09/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UITableViewController {
+    
+    private let cellID = "cell"
+    private var tasks: [Task] = []
+    private let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        
+        // Table view cell register
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchData()
+    }
+
+    /// Setup view
+    private func setupView() {
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    /// Setup navigation bar
+    private func setupNavigationBar() {
+        
+        // Set title for navigation bar
+        title = "Tasks list"
+        
+        // Title color
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        // Navigation bar color
+        navigationController?.navigationBar.barTintColor = UIColor(
+            displayP3Red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        // Set large title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Add button to navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Add",
+            style: .plain,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        showAlert(title: "New Task", message: "What do you want to do?")
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else {
+                print("The text field is empty")
+                return
+            }
+            
+            // Add new task to tasks array
+            self.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        
+        // Entity name
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: managedContext) else { return }
+        
+        // Model instance
+        let task = NSManagedObject(entity: entityDescription, insertInto: managedContext) as! Task
+        
+        task.name = taskName
+        
+        do {
+            try managedContext.save()
+            tasks.append(task)
+            self.tableView.insertRows(
+                at: [IndexPath(row: self.tasks.count - 1, section: 0)],
+                with: .automatic
+            )
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func fetchData() {
+        
+        // Запрос выборки из базы всех значений по ключу Task
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let editNotesAction = UITableViewRowAction(style: .default, title: "Edit") { (action: UITableViewRowAction, indexPath: IndexPath) in
+            
+            let task = self.tasks[indexPath.row]
+            
+            let alert = UIAlertController(title: "Edit", message: "", preferredStyle: .alert)
+            
+            let saveAction = UIAlertAction(title: "Save", style: .default, handler: { (action) in
+                guard let textField = alert.textFields?.first else { return }
+                task.name = textField.text
+                do {
+                    try self.managedContext.save()
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+                tableView.reloadData()
+            })
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+            alert.addTextField()
+            alert.textFields?.first?.text = task.name
+            alert.addAction(saveAction)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true)
+        }
+        
+        editNotesAction.backgroundColor = .blue
+        
+        let deleteNotesAction = UITableViewRowAction(style: .default, title: "Delete") { (action: UITableViewRowAction, indexPath: IndexPath) in
+            
+            self.managedContext.delete(self.tasks[indexPath.row])
+            
+            self.tasks.remove(at: indexPath.row)
+            
+            do {
+                try self.managedContext.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        return [deleteNotesAction, editNotesAction]
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        
+        return cell
+    }
+}
+
